e98b8d5b126bd0c6b546ff6d99ded3cdb58e47a8
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-02 16:21:48 +0530

    print PartTime Employee wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -28,3 +28,24 @@ then
 fi
 echo "Daily Wage of an Employee= " $salary
 
+
+
+#Add Part time Employee & Wage
+
+
+echo "Welcome to Employee Wage Computation"
+
+Employee present or not
+
+isPresent=1;
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then 
+         empRatePerHr=20;
+         empHrs=8;
+         salary=$(( $empHrs*$empRatePerHr )) 
+   else
+      salary=0;
+fi
+echo "Daily Wage of an Employee= " $salary
