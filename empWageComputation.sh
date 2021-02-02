f45eac5dc7f3e7cd06e25ff4866140d5c1c84d96
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-02 15:57:13 +0530

    add attendance function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Employee Wage Computation"
+
+
+#Check Employee is Present or Absent
+
+echo "Employee Wage Computation"
+attend=$(( $RANDOM%2 ))
+if (( $attend==0 ))
+then
+	echo "Employee is Absent"
+else
+	echo "Employee  is Present"
+fi
+
