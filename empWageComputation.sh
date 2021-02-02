bcd5ddfe38cd0e8c3f54022729d899f812b1a606
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-02 16:58:11 +0530

    print employee wages using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -49,3 +49,30 @@ then
       salary=0;
 fi
 echo "Daily Wage of an Employee= " $salary
+
+
+
+#Solving using Switch Case Statement
+
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+          $isFullTime)
+                    empHrs=8
+                        ;;
+                  $isPartTime)
+                        empHrs=4
+                            ;;
+               *)
+               empHrs=0
+                   ;;
+esac
+
+  salary=$(($empHrs*$empRatePerHr)) 
+
+echo "Salary Of Employee= "$salary  
+
