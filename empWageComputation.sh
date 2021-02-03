1714323b96f9ab2d9483ba3db2fd4bd0a02ff350
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-03 16:58:58 +0530

    find employee wage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -79,6 +79,7 @@ echo "Salary Of Employee= "$salary
 
 
 
+<<<<<<< HEAD
 #Calculate Wages till a condition of total working hours or days is reached for a month
 
 
@@ -96,4 +97,42 @@ echo  "Salary of an Employee" $totalSalary
 
 
 
+#Calculating Wages for a Month
 
+
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+function getWorkingHours(){
+     case $1 in
+         $IS_FULL_TIME)
+          workHours=8
+                   ;;
+        $IS_PART_TIME)
+             workHours=4
+                   ;;
+      *)
+  workHours=0
+      ;;
+esac
+echo $workHours
+}
+
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]] 
+do
+  (( totalWorkingDays++ ))
+        workHours=$( getWorkingHours $((RANDOM%3)) )
+        totalWorkHours=$(($totalWorkHours+$workHours))
+
+ done
+
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
+
+echo "salary of an Employee= "$totalSalary
+>>>>>>> Calculating_Wages_For_Month_UC
