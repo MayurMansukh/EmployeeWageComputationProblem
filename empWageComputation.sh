26e1502619124d7def554ed95b283da0125eb3a4
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-03 18:41:24 +0530

    find Days and Daily Wage with TotalWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -230,3 +230,51 @@ echo "All Keys " ${!empDailyWage[@]}
 
 
 
+#Store the Day and the Daily Wage along with the Total Wage
+
+
+
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+function getWorkingHours(){
+     case $1 in
+         $IS_FULL_TIME)
+          workHours=8
+                   ;;
+        $IS_PART_TIME)
+             workHours=4
+                   ;;
+      *)
+  workHours=0
+      ;;
+esac
+echo $workHours
+}
+
+function calcDailyWage(){
+          local workHrs=$1
+           wage=$(($workHrs+$EMP_RATE_PER_HR))
+           echo $wage
+}
+
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+  (( totalWorkingDays++ ))
+        workHours=$( getWorkingHours $((RANDOM%3)) )
+        totalWorkHours=$(($totalWorkHours+$workHours))
+         empDailyWage[$totalWorkingDays]=$( calcDailyWage $workHours )
+
+ done
+totalwages=$(($totalWorkHours*$EMP_RATE_PER_HR));
+totalSalary=$( calcDailyWage $totalWorkHours )
+echo "days $totalWorkingDays"
+echo "total wage  $totalwages"
+echo "Daily Wage " ${empDailyWage[@]}
+echo "All Keys " ${!empDailyWage[@]}
