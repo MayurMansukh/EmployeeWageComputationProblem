4e229291e72e878165fb2d0289263299a317b3c5
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-05 13:42:59 +0530

    find daily wages

--- Emp_Wage_Computation.sh
+++ Emp_Wage_Computation.sh
@@ -1,13 +1,16 @@
 #! /bin/bash -x
 
-#Check Employee is Present or Absent
+#Calculate Daily Employee Wage
 
-echo "Employee Wage Computation"
-attend=$(( $RANDOM%2 ))
-if (( $attend==0 ))
-then
-	echo "Employee is Absent"
-else
-	echo "Employee  is Present"
+isPresent=1;
+randomCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $randomCheck ]
+then 
+         empRatePerHr=20;
+         empHrs=8;
+         salary=$(( $empHrs*$empRatePerHr )) 
+   else
+      salary=0;
 fi
-
+echo "Daily Wage of an Employee= " $salary
